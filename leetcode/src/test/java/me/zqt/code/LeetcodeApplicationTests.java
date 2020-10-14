5f9eb573f43605b667f511c439c647cd81b7b311
zqtao <zqtao23@163.com>
2020-10-14 13:54:00 +0800

    LeetCode

---
+++ leetcode/src/test/java/me/zqt/code/LeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zqt.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
